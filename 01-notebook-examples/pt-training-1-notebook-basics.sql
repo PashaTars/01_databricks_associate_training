60ce87ade6382d2739adb1821c4385c599a62755
PashaTars <PashaTars>
2023-05-24 04:36:43 +0000

    Initial notebook upload

---
+++ 01-notebook-examples/pt-training-1-notebook-basics.sql
@@ -0,0 +1,23 @@
+-- Databricks notebook source
+-- DBTITLE 0,Create and manage interactive cluster
+-- MAGIC %python
+-- MAGIC print("press shift+Enter")
+
+-- COMMAND ----------
+
+SELECT "press shift+Enter"
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Use %md command to create markdown
+-- MAGIC # Use # to change size (# bigger *size*) (### smaller size)
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC display(dbutils.fs.ls('/databricks-datasets'))
+
+-- COMMAND ----------
+
+
